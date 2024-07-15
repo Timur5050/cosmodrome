890a23cdf938b963ee9e63d38d9a6f7975b497ae
pro <pro@pros-MacBook-Pro.local>
2024-07-15 07:56:09 +0300

    changed flight model and added str representation for each model

--- hangar/models.py
+++ hangar/models.py
@@ -7,12 +7,26 @@ class Racket(models.Model):
     speed = models.IntegerField()
     destination = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return {self.name}
+
 
 class Astronaut(AbstractUser):
     year_of_experience = models.IntegerField()
 
+    def __str__(self):
+        return self.username
+
 
 class Flight(models.Model):
+    name = models.CharField(max_length=255)
     duration = models.IntegerField()
     astronauts = models.ManyToManyField(Astronaut, related_name="flights")
     racket = models.ForeignKey(Racket, on_delete=models.CASCADE, related_name="flights")
+    flight_date = models.DateField()
+
+    def __str__(self):
+        return self.name
