c5acb24a283782cedcf0544508a809f780fae072
pro <pro@pros-MacBook-Pro.local>
2024-07-15 10:57:21 +0300

    creted views for creating, representating of flight, astronauts and rackets

--- hangar/views.py
+++ hangar/views.py
@@ -3,8 +3,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
-from hangar.forms import UserCreationForm, AstronautCreationForm, RacketForm
-from hangar.models import Racket
+from hangar.forms import UserCreationForm, AstronautCreationForm, RacketForm, FlightForm
+from hangar.models import Racket, Astronaut, Flight
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -31,7 +31,7 @@ def user_creation_view(request: HttpRequest) -> HttpResponse:
             return render(request, "hangar/user_form.html", context=context)
 
 
-def racket_view(request: HttpRequest) -> HttpResponse:
+def racket_list_view(request: HttpRequest) -> HttpResponse:
     queryset = Racket.objects.all()
     context = {
         "racket_list": queryset
@@ -50,3 +50,33 @@ def racket_create_view(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             form.save()
             return redirect(reverse_lazy("hangar:racket-list"))
+
+
+def astronaut_list_view(request: HttpRequest) -> HttpResponse:
+    queryset = Astronaut.objects.all()
+    context = {
+        "astronaut_list": queryset
+    }
+    return render(request, "hangar/astronaut_list.html", context=context)
+
+
+def flight_list_view(request: HttpRequest) -> HttpResponse:
+    queryset = Flight.objects.all()
+    context = {
+        "flight_list": queryset
+    }
+    return render(request, "hangar/flight_list.html", context=context)
+
+
+def flight_list_create(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        context = {
+            "form": FlightForm
+        }
+        return render(request, "hangar/flight_form.html", context=context)
+
+    if request.method == "POST":
+        form = FlightForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy("hangar:main-page"))
