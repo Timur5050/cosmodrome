8fb8bba75a15675fb98035350c09e4fb5b5d7b97
pro <pro@pros-MacBook-Pro.local>
2024-07-15 07:52:34 +0300

    created hangar model

--- hangar/models.py
+++ hangar/models.py
@@ -11,3 +11,8 @@ class Racket(models.Model):
 class Astronaut(AbstractUser):
     year_of_experience = models.IntegerField()
 
+
+class Flight(models.Model):
+    duration = models.IntegerField()
+    astronauts = models.ManyToManyField(Astronaut, related_name="flights")
+    racket = models.ForeignKey(Racket, on_delete=models.CASCADE, related_name="flights")
