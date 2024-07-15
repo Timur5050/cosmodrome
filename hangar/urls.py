c27a26ef3dbe93658ef0ba948dc51d3d687b3e16
pro <pro@pros-MacBook-Pro.local>
2024-07-15 09:47:35 +0300

    created paths for registrations, rackets list and rackets creation

--- hangar/urls.py
+++ hangar/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from hangar.views import index
+from hangar.views import index, user_creation_view, racket_create_view, racket_view
 
 
 urlpatterns = [
-    path("", index, name="main-page")
+    path("", index, name="main-page"),
+    path("registration/", user_creation_view, name="registration"),
+    path("rackets/", racket_view, name="racket-list"),
+    path("rackets/create/", racket_create_view, name="racket-create"),
 ]
 
 app_name = "hangar"
