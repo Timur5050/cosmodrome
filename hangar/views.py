3e7099d0b7349ef872d45706097a32df2b050dd3
pro <pro@pros-MacBook-Pro.local>
2024-07-17 13:12:40 +0300

    deleted useless imports

--- hangar/views.py
+++ hangar/views.py
@@ -3,12 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, DetailView, ListView, CreateView
 
 from hangar.forms import (
-    UserCreationForm,
     AstronautSearchForm,
     RacketForm,
     FlightForm,
